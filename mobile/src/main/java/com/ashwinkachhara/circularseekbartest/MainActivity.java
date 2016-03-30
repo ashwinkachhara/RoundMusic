d007395ddaac700c9e21c6859f7342cb4705c955
Ashwin Kachhara <kachhara.ashwin@gmail.com>
2016-03-30 09:53:08 -0400

    Added song list to mobile

--- mobile/src/main/java/com/ashwinkachhara/circularseekbartest/MainActivity.java
+++ mobile/src/main/java/com/ashwinkachhara/circularseekbartest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ashwinkachhara.circularseekbartest;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
@@ -17,6 +20,8 @@ import com.devadvance.circularseekbar.CircularSeekBar;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,9 +47,22 @@ public class MainActivity extends AppCompatActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                audioPlayer("mnt/sdcard/Music","01 - Step Out.mp3");
+                audioPlayer("mnt/sdcard/Music", "01 - Step Out.mp3");
             }
         });
+
+        songView = (ListView)findViewById(R.id.song_list);
+        songList = new ArrayList<Song>();
+        getSongList();
+
+        Collections.sort(songList, new Comparator<Song>() {
+            public int compare(Song a, Song b) {
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+
+        SongAdapter songAdt = new SongAdapter(this, songList);
+        songView.setAdapter(songAdt);
     }
 
     @Override
@@ -100,6 +118,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         mp.start();
+
+    }
+
+    public void getSongList() {
+        //retrieve song info
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+        }
+
+
 
     }
 }
